3492c7189461cb28291730bc37e1aba8e907c213
yongtianyong <yongtianyong@nibirutech.com>
2013-09-13 22:04:44 +0800

    fix bug

--- scripts/firewall/iptables.sh
+++ scripts/firewall/iptables.sh
@@ -26,8 +26,8 @@ check_ru(){
     [[ ! -d $dir_r ]] && echo " wrong rule dir" && exit 1
     [[ -z `ls $dir_r/*.txt 2> /dev/null ` ]] && echo " no rule file found in $dir_r" && exit 1
 }
-fw_start(){
-    cat <<EOF
+add_default(){
+        cat <<EOF
 /sbin/iptables -t nat -F
 /sbin/iptables -t nat -P PREROUTING ACCEPT
 /sbin/iptables -t nat -P POSTROUTING ACCEPT
@@ -37,13 +37,20 @@ fw_start(){
 /sbin/iptables -t filter -P INPUT DROP
 /sbin/iptables -t filter -P FORWARD ACCEPT
 /sbin/iptables -t filter -P OUTPUT ACCEPT
-#enable ssh and related
+/sbin/iptables -t filter -A INPUT -i lo -j ACCEPT
+/sbin/iptables -t filter -A OUTPUT -o lo -j ACCEPT
+#enable ping,ssh and related
+/sbin/iptables -t filter -A INPUT -p icmp -j ACCEPT
+/sbin/iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 /sbin/iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
 /sbin/iptables -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/iptables -t filter -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/iptables -t filter -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 #additional rules
 EOF
+}
+fw_start(){
+    add_default;
     for file in `ls $dir_r/*.txt`; do
       awk -F: '
         function print_err(a,b)
@@ -111,30 +118,16 @@ EOF
 }
 #
 fw_stop(){
-/sbin/iptables -t nat -F
-/sbin/iptables -t nat -P PREROUTING ACCEPT
-/sbin/iptables -t nat -P POSTROUTING ACCEPT
-/sbin/iptables -t mangle -F
-/sbin/iptables -t filter -F
-#default rule
-/sbin/iptables -t filter -P INPUT DROP
-/sbin/iptables -t filter -P FORWARD ACCEPT
-/sbin/iptables -t filter -P OUTPUT ACCEPT
-#enable ssh and related
-/sbin/iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
-/sbin/iptables -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -t filter -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -t filter -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-
+    add_default|bash
 }
 #get args
 if  [ $# -ne 1 ];then
     Usage
 fi
 case $1 in
-    start)      check_uid;check_ru;fw_start;;
-    reload)     check_uid;fw_stop;check_ru;fw_start;;
-    stop)	check_uid;fw_stop;;
-    echo)	fw_start;;
+    start)      check_uid;check_ru;fw_start|bash;;
+    reload)     check_uid;check_ru;fw_stop;fw_start|bash;;
+    stop)	check_uid;fw_stop|bash;;
+    echo)	check_uid;check_ru;fw_start;;
     *)          Usage;;
 esac
